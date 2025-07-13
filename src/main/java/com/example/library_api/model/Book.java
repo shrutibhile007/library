6a5e9ae9fa3ec30e9b27ae2eeaded9d2cbfd8053
shrutibhile007 <shruti.bhile@gmail.com>
2025-07-13 15:55:25 +0530

    Update Book.java

---
+++ src/main/java/com/example/library_api/model/Book.java
@@ -0,0 +1,36 @@
+package com.example.library_api.model;
+
+public class Book {
+    private Integer id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    public Book() {}
+
+    public Book(Integer id, String title, String author, String isbn, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    // Getters and Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+    
+}
